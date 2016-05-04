e7e671b5c92b68717c823bb6720fb02ef09fa744
Pablo Casares <pcasares@redborder.net>
2016-05-04 15:52:52 +0200

    First commit

---
+++ src/main/java/Problem3.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Lito on 29/4/16.
+ */
+public class Problem3 {
+
+    Problem3() {
+        long number = 600851475143L;
+
+        for (long i = 1; i < number; i++) {
+
+            if (number % i == 0 && findPrime(i)) {
+                System.out.println("The number: " + i + " is prime");
+            }
+        }
+    }
+
+    static boolean findPrime(long number) {
+        boolean isPrime = true;
+        for (long i = 1; i < number; i++) {
+            if (i != 1L && i != number && number % i == 0) {
+                isPrime = false;
+            }
+        }
+        return isPrime;
+    }
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Lito on 29/4/16.
+ */
+public class main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
